1a7879f4918e4c7e5048270742811ca74c816eaa
israelvargasjimenez <israel.vargas.jimenez@hotmail.com>
2020-06-21 18:31:04 +0200

    se añade la libreria al classpath

--- src/com/ivj/sql/Main.java
+++ src/com/ivj/sql/Main.java
@@ -5,16 +5,18 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) {
-		MySQL mysql = new MySQL("192.168.1.5", "3306", "pildoras", "elisa", "Sismed08");		
+		//MySQL mysql = new MySQL("192.168.1.5", "3306", "pildoras", "elisa", "Sismed08");		
 		
-		mysql.select("*", "pedidos", null,null);
+		//mysql.select("*", "pedidos", null,null);
 		//mysql.select("*", "productos", null, null);
 		
 		//mysql.select("PAIS_ORIGEN", "productos", null, null);
 		
 		//mysql.select("*", "productos", "SECCION","'CONFECCI�N'");
 		
-		mysql.printSelectResults();
+		//mysql.printSelectResults();
+		
+		
 
 	}
 
