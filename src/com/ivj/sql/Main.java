e93da9109fd72e469cb494b631b5e5651fe2c9a8
israelvargasjimenez <israel.vargas.jimenez@hotmail.com>
2020-06-21 08:51:51 +0200

    se sigue

--- src/com/ivj/sql/Main.java
+++ src/com/ivj/sql/Main.java
@@ -7,10 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		MySQL mysql = new MySQL("192.168.1.5", "3306", "pildoras", "elisa", "Sismed08");		
 		
-		//mysql.select("CODIGO_CLIENTE, FECHA_PEDIDO", "pedidos", null);
+		mysql.select("CODIGO_CLIENTE, FECHA_PEDIDO", "pedidos", null,null);
 		//mysql.select("*", "productos", null, null);
 		
-		mysql.select("PAIS_ORIGEN", "productos", null, null);
+		//mysql.select("PAIS_ORIGEN", "productos", null, null);
 		
 		//mysql.select("*", "productos", "SECCION","'CONFECCIÓN'");
 		
