f9ca21e33ad510e234aab4ab1b7af9265ba358a0
israelvargasjimenez <israel.vargas.jimenez@hotmail.com>
2020-06-21 18:54:46 +0200

    No se ha conseguido conectar con sql server

--- src/com/ivj/sql/Main.java
+++ src/com/ivj/sql/Main.java
@@ -1,6 +1,10 @@
 package com.ivj.sql;
 
+
 import java.util.HashMap;
+
+
+import java.sql.*;
 
 public class Main {
 
@@ -16,7 +20,20 @@ public class Main {
 		
 		//mysql.printSelectResults();
 		
-		
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String url = "jdbc:sqlserver://ESTUDIOSISRAEL\\MSSQLSERVER_NEW:1433;databaseName =Mercado;user=usuarioSQL;password=Sismed08;";
+		Connection conexion;
+	    try {
+			conexion = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
